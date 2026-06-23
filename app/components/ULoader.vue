c9ac73fa36449b0659517f12ed6d6cc8ee8a7dfb
sardorceeksamurai@gmail.com <sardorceeksamurai@gmail.com>
2026-06-24 00:21:03 +0500

    fix merchant acc

---
+++ app/components/ULoader.vue
@@ -0,0 +1,30 @@
+﻿<template>
+  <div class="u-loader">
+    <div class="u-loader__spinner"></div>
+  </div>
+</template>
+
+<style scoped>
+.u-loader {
+  width: 100%;
+  min-height: 160px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.u-loader__spinner {
+  width: 32px;
+  height: 32px;
+  border: 3px solid rgba(120, 120, 120, 0.25);
+  border-top-color: rgba(120, 120, 120, 0.9);
+  border-radius: 999px;
+  animation: u-loader-spin 0.8s linear infinite;
+}
+
+@keyframes u-loader-spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
